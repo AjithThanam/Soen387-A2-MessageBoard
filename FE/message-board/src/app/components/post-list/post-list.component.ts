ef8e6e0edd526e7cae415172bca76e3f77a97d2d
rThamb <ren180197@hotmail.com>
2020-11-07 03:19:00 -0500

    Binded start and end for filter post.

--- FE/message-board/src/app/components/post-list/post-list.component.ts
+++ FE/message-board/src/app/components/post-list/post-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Post } from '../../models/Post';
 import { PostService } from '../../services/post/post.service';
 
+import { FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
@@ -11,29 +13,28 @@ export class PostListComponent implements OnInit {
 
   currentPosts: Post[];
 
+  start = new FormControl();
+  end = new FormControl();
+
   constructor(private service: PostService) { }
 
   ngOnInit(): void {
-    this.service.getPost(null, null,null).subscribe((posts) => this.currentPosts = posts);
+    this.findMostRecentPost();
   }
 
 
   //  5 different types of filter - by on User Posted, date range (start and end) 
-  loadPost(){
 
-  }
-
-  findMyPost(){
+  findMyPosts(){
 
   }
 
   findPostByDateRange(){
+    alert(this.start.value + " " + this.end.value);
 
   }
-
 
   findMostRecentPost(){
-
+     this.service.getPost(null, null,null).subscribe((posts) => this.currentPosts = posts);
   }
-
 }
